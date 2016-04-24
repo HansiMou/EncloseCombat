03a145732de30a457ffdb4f41b1089df5cec1174
HansiMou <mhsasd@126.com>
2016-04-23 22:02:20 -0400

    ？？？

--- ts_output_readonly_do_NOT_change_manually/src/game.js
+++ ts_output_readonly_do_NOT_change_manually/src/game.js
@@ -244,7 +244,6 @@ angular.module('myApp', ['ngTouch', 'ui.bootstrap', 'gameServices'])
                     game.moves.push({ row: row, col: col });
                 }
                 log.info(angular.toJson(game.moves));
-                draggingLines.style.webkitTransform = 'scale(1)';
                 dragDone();
                 draggingLines.style.webkitTransform = 'scale(1)';
             }
