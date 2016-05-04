1d5a64aefa7b26842986c1aacfd18bb9db66ac65
HansiMou <mhsasd@126.com>
2016-05-04 18:37:12 -0400

    U

--- Gruntfile.js
+++ Gruntfile.js
@@ -185,7 +185,7 @@ module.exports = function(grunt) {
             'imgs/HelpSlide2.png',
           ],
           network: 
-            '*'
+            ['*']
           ,
           timestamp: true
         },
